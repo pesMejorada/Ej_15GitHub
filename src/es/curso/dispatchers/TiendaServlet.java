7e94facf77b69b2d32d580c371c7d7a4652d3d11
Usuario <Usuario@aula2-PC2>
2015-05-26 14:10:20 +0200

    Add disptacher code

--- src/es/curso/dispatchers/TiendaServlet.java
+++ src/es/curso/dispatchers/TiendaServlet.java
@@ -1,6 +1,8 @@
 package es.curso.dispatchers;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,10 +10,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
+import es.curso.controllers.ejb.DarAltaClienteControllerEjb;
+import es.curso.model.entity.Cliente;
+
 /**
  * Servlet implementation class TiendaServlet
  */
-@WebServlet("/Tienda")
+@WebServlet("/Tienda/*")
 public class TiendaServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -33,14 +39,63 @@ public class TiendaServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-	}
+		 String action = request.getPathInfo().substring(1);
+		 request.setCharacterEncoding("UTF-8");
+		 switch(action){
+		     case  "listarTodos":  // se invocará al controllador adecuado
+		    	                   // q obtendrá todos los clientes
+		    	                  //esta peticon redirije a otra pagina.
+		    	                  break;
+		     case "buscarPorNombre": // se invocará al controlador que haga
+		    	                     // la consulta por nombre, que obtendrá
+		    	                     // solo los clientes que coincidan con el nombre buscado
+		    	                     // esta peticion redirije a otra pagina
+		    	                   break;  	                  
+		 }
+		  // tengo q redirigir hacia una vista jsp para mostrar los clientes
+		   
+		 RequestDispatcher  rd; 
+		 // de laguna manera haya q enviarle a la vista el resultado de la consulta a la
+		  // base de datos.... 
+		 //this.getServletContext().getRequestDispatcher("/jsp" +"listarTodos.jsp").forward(request, response);;
+		 rd = request.getRequestDispatcher("/jsp/listarTodos.jsp");
+		 rd.forward(request, response);
+		}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		String action = request.getPathInfo().substring(1);
+		request.setCharacterEncoding("UTF-8");
+		switch(action){
+		case "altaCliente":
+			                 // recuperar los datos tecleados en el formulario
+			                 String nombres = request.getParameter("nombres");
+			                 String apellidos = request.getParameter("apellidos");
+			                 String dni = request.getParameter("dni");
+			                 Cliente cliente = new Cliente(0, nombres, apellidos, dni);
+			                 // invocará al controlador adecuado
+			                 DarAltaClienteControllerEjb controlador= new DarAltaClienteControllerEjb();
+			                 controlador.agregar(cliente);
+			                break;
+			}
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
